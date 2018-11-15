80e7ca726aec104cb3283b40613f5e8bcab4c8f3
Henrik <31667488+henriksb@users.noreply.github.com>
2018-11-15 20:19:18 +0100

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 if [ "$EUID" -ne 0 ]
 	then echo "[!] Please run as root"
@@ -8,4 +8,4 @@ fi
 mv RouterPassword.py /usr/local/bin/RouterPassword
 mv RouterScrape.py /usr/local/bin/RouterScrape.py
 chmod +x /usr/local/bin/RouterPassword
-echo "[+] Setup was successful. The script can now be run from anywhere by typing 'RouterPasswor'"
+echo "[+] Setup was successful. The script can now be run from anywhere by typing 'RouterPassword'"
